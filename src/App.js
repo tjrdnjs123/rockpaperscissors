ab1c5dfccae4dd0d3dee35211b7a8ad44120715b
Seokwon Choi <cswcoco03112@gmail.com>
2024-03-20 21:33:40 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -10,28 +10,62 @@ import { useState } from "react";
 //6.이기면 초록 지면 빨강 비기면 검정 박스테두리
 const choice = {
   rock: {
-    name: "Rock",
+    name: "rock",
     img: "https://imageio.forbes.com/specials-images/imageserve/dv424076/Boulder--Namibia--Africa/960x0.jpg?format=jpg&width=960",
   },
   scissors: {
-    name: "Scissors",
+    name: "scissors",
     img: "https://res.cloudinary.com/rsc/image/upload/bo_1.5px_solid_white,b_auto,c_pad,dpr_2,f_auto,h_399,q_auto,w_710/c_pad,h_399,w_710/C0487101-01?pgw=1",
   },
   paper: {
-    name: "Paper",
+    name: "paper",
     img: "https://5.imimg.com/data5/HC/KF/HZ/SELLER-4619430/white-art-paper.jpg",
   },
 };
 function App() {
   const [userSelect, setUserSelect] = useState(null);
-  const play = (shape) => {
-    setUserSelect(choice[shape]);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [computerResult, setComputerResult] = useState("");
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let ComputerChoice = randomChoice();
+    setComputerSelect(ComputerChoice);
+    let userJudgement = judgement(choice[userChoice], ComputerChoice);
+    setResult(userJudgement);
+    setComputerResult(computerJudgement(userJudgement));
   };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "Tie";
+    } else if (user.name === "rock") {
+      return computer.name === "scissors" ? "Win" : "Lose";
+    } else if (user.name === "scissors") {
+      return computer.name === "paper" ? "Win" : "Lose";
+    } else if (user.name === "paper") {
+      return computer.name === "rock" ? "Win" : "Lose";
+    }
+  };
+  const computerJudgement = (userJudgement) => {
+    if (userJudgement === "Tie") {
+      return "Tie";
+    } else {
+      return userJudgement === "Win" ? "Lose" : "Win";
+    }
+  };
+
   return (
     <div>
       <div className="main">
-        <Box name="You" item={userSelect} />
-        <Box name="Computer" />
+        <Box name="You" item={userSelect} result={result} />
+        <Box name="Computer" item={computerSelect} result={computerResult} />
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -3,10 +3,10 @@ import React from "react";
 const Box = (props) => {
   console.log(props);
   return (
-    <div className="box">
+    <div className={`box ${props.result}`}>
       <h1>{props.name}</h1>
       <img className="item-img" src={props.item && props.item.img} />
-      <h2>Win</h2>
+      <h2>{props.result}</h2>
     </div>
   );
 };
